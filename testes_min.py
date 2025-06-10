fad4703a9886691ef1b66e662dea4c796da6b660
Vinicius Centurion <43175480+vncenturion@users.noreply.github.com>
2025-06-10 01:03:46 -0300

    Update testes_min.py

--- testes_min.py
+++ testes_min.py
@@ -1,11 +1,12 @@
-#EXEMPLO 01
+#EXEMPLO 01 MIN
+#O codigo não trata os problemas de minimização de forma automática.
 
 func_objetivo = [3, 2, 0, 0]  # x1, x2, s1, s2
 matriz_restricoes = np.array([
-    [1, 1, 1, 0],
+    [1, 2, 1, 0],
     [2, 1, 0, 1]
 ], dtype=float)
 vetor_restricoes = np.array([4, 5], dtype=float)
 
 # Rodar o simplex com tipo 'min'
-simplex(func_objetivo, matriz_restricoes, vetor_restricoes, tipo='min')
+resultado=simplex(func_objetivo, matriz_restricoes, vetor_restricoes, tipo='max')
